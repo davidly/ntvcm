b62a1cdfe09ea8e9055897e27683a1ad125aea32
davidly <davidly_lee@hotmail.com>
2023-07-05 14:46:25 -0700

    name g++ binaries with g appended

--- mg.bat
+++ mg.bat
@@ -7,6 +7,6 @@ rem With RSS
 g++ -Ofast -ggdb -fopenmp -D NTVCM_RSS_SUPPORT -D _MSC_VER -D _GNU_CPP ntvcm.cxx x80.cxx -I ../djl -D DEBUG -o ntvcmg.exe -static -lwininet
 
 rem Without RSS
-rem g++ -Ofast -ggdb -D _MSC_VER -D _GNU_CPP ntvcm.cxx x80.cxx -I ../djl -D DEBUG -o ntvcm.exe -static
+rem g++ -Ofast -ggdb -D _MSC_VER -D _GNU_CPP ntvcm.cxx x80.cxx -I ../djl -D DEBUG -o ntvcmg.exe -static
 
 
--- mgr.bat
+++ mgr.bat
@@ -7,7 +7,7 @@ rem With RSS
 g++ -Ofast -ggdb -fopenmp -D NTVCM_RSS_SUPPORT -D _MSC_VER -D _GNU_CPP ntvcm.cxx x80.cxx -I ../djl -D NDEBUG -o ntvcmg.exe -static -lwininet
 
 rem Without RSS
-g++ -Ofast -ggdb -D _MSC_VER -D _GNU_CPP ntvcm.cxx x80.cxx -I ../djl -D NDEBUG -o ntvcm.exe -static
+rem g++ -Ofast -ggdb -D _MSC_VER -D _GNU_CPP ntvcm.cxx x80.cxx -I ../djl -D NDEBUG -o ntvcmg.exe -static
 
 
 
