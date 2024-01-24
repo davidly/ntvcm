1785ff71e261d34a683a72834db6f2031bea5775
davidly <davidly_lee@hotmail.com>
2024-01-23 16:34:08 -0800

    build static so binaries can be moved

--- m.sh
+++ m.sh
@@ -1,5 +1,5 @@
 # with RSS
-g++ -ggdb -Og -fno-builtin -D NTVCM_RSS_SUPPORT -D DEBUG -I . ntvcm.cxx x80.cxx -lssl -lcrypto -o ntvcm 
+g++ -ggdb -Og -fno-builtin -D NTVCM_RSS_SUPPORT -D DEBUG -I . ntvcm.cxx x80.cxx -lssl -lcrypto -o ntvcm  -static
 
 # without RSS
-# g++ -ggdb -Ofast -fno-builtin -D DEBUG -I . ntvcm.cxx x80.cxx -o ntvcm 
+# g++ -ggdb -Ofast -fno-builtin -D DEBUG -I . ntvcm.cxx x80.cxx -o ntvcm -static
