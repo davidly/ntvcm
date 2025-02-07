a12ccdb90d9eba3e7d71e80e64c8ac654c2e6b26
mike632t <1799044+mike632t@users.noreply.github.com>
2025-02-07 20:19:06 +0000

    Does not attempt to get commit id

--- mr.bat
+++ mr.bat
@@ -1,11 +1,5 @@
-
-@ECHO OFF
-FOR /F "tokens=*" %%s IN ('git log -1 HEAD --format^=%%h ') DO (SET COMMIT=%%s)
-rem ECHO %COMMIT%
-
 rem with RSS
-cl /W4 /wd4706 /wd4996 /nologo ntvcm.cxx x80.cxx /DNDEBUG /DCOMMIT_ID=%COMMIT% /DNTVCM_RSS_SUPPORT /openmp /I. /GS- /GL /Oti2 /Ob3 /Qpar /Fa /FAsc /EHac /Zi /jumptablerdata /D_AMD64_ /link user32.lib /OPT:REF
+cl /W4 /wd4706 /wd4996 /nologo ntvcm.cxx x80.cxx /DNDEBUG /DNTVCM_RSS_SUPPORT /openmp /I. /GS- /GL /Oti2 /Ob3 /Qpar /Fa /FAsc /EHac /Zi /jumptablerdata /D_AMD64_ /link user32.lib /OPT:REF
 
 rem without RSS
-rem cl /W4 /wd4706 /wd4996 /nologo ntvcm.cxx x80.cxx /DNDEBUG /DCOMMIT_ID=%COMMIT% /I. /GS- /GL /Oti2 /Ob3 /Qpar /Fa /FAsc /EHac /Zi /jumptablerdata /D_AMD64_ /link user32.lib ntdll.lib /OPT:REF
-
+rem cl /W4 /wd4706 /wd4996 /nologo ntvcm.cxx x80.cxx /DNDEBUG /I. /GS- /GL /Oti2 /Ob3 /Qpar /Fa /FAsc /EHac /Zi /jumptablerdata /D_AMD64_ /link user32.lib ntdll.lib /OPT:REF
