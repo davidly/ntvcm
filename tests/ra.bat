95f5debbf8d222ef70187a3b3c97452456ae74ec
davidly <davidly_lee@hotmail.com>
2025-02-07 16:07:23 -0800

    add back cpu tests

---
+++ tests/ra.bat
@@ -0,0 +1,25 @@
+echo test 8080ex1.com
+ntvcm -8 8080ex1.com
+
+echo:
+echo test 8080pre.com
+ntvcm -8 8080pre.com
+
+echo:
+echo test tst8080.com
+ntvcm -8 tst8080.com
+
+echo:
+echo test test.com
+ntvcm -8 test.com
+
+echo:
+echo test cputest in 8080 mode
+ntvcm -8 cputest.com
+
+echo:
+echo test cputest in Z80 mode
+ntvcm cputest.com
+echo:
+
+
---
+++ tests/rai.sh
@@ -0,0 +1,6 @@
+../../rvos/rvos -h:10 ../ntvcm -8 8080ex1.com
+../../rvos/rvos -h:10 ../ntvcm -8 8080pre.com
+../../rvos/rvos -h:10 ../ntvcm -8 tst8080.com
+../../rvos/rvos -h:10 ../ntvcm -8 test.com
+../../rvos/rvos -h:10 ../ntvcm -8 cputest.com
+../../rvos/rvos -h:10 ../ntvcm cputest.com
